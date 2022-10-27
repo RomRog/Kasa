d615b9bf88c82584d259f56576dda20174ea4f1f
Romain ROGUET <romain.roguet@gmail.com>
2022-10-27 21:11:48 +0200

    page corrige

--- kasa/src/pages/about.js
+++ kasa/src/pages/about.js
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import AboutMainText from '../components/about/about-main-text';
 import BackgroundAbout from '../components/about/background-about';
-import Footer from '../components/footer';
 import Logo from '../components/logo';
 import Navigation from '../components/navigation';
 
--- kasa/src/pages/home.js
+++ kasa/src/pages/home.js
@@ -2,8 +2,6 @@ import { Component } from 'react';
 import Footer from '../components/footer';
 import BackgroundHome from '../components/home/background-home';
 import GridHomePage from '../components/home/gril-home-page';
-import Logo from '../components/logo';
-import Navigation from '../components/navigation';
 
 class Home extends Component {
     render() {
